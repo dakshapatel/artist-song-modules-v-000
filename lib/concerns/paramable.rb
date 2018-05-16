12bd07c8fc28bef7e0b5adf97eecb6ce161b4902
Daksha Patel <dakshapatel100@gmail.com>
2018-05-16 16:37:59 +0000

    automatically backed up by learn

--- lib/concerns/paramable.rb
+++ lib/concerns/paramable.rb
@@ -1,10 +1,8 @@
 module Paramable
   module InstanceMethod
-    def to_para
       #converts its name to a url friendly parameter
      def to_param
        name.downcase.gsub(' ', '-')
      end
-   end
  end
 end
