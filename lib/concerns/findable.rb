29fd3b88ff81f5db49e8ed81dc4727b335f59da5
Daksha Patel <dakshapatel100@gmail.com>
2018-05-16 16:30:20 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,20 +1,29 @@
 module Findable
 
 
+  module ClassMethods
+    def find_by_name
+      #can find a song by name
+      def self.find_by_name(name)
+        #@@songs.detect{|a| a.name == name}
+        all.detect{|a| a.name == name}
+      end
 
-def find_by_name
-  #can find a song by name
-  def self.find_by_name(name)
-    #@@songs.detect{|a| a.name == name}
-    all.detect{|a| a.name == name}
+      #can find an artist by name
+      def self.find_by_name(name)
+        #@@artist.detect{|a| a.name == name}
+        all.detect{|a| a.name == name}
+      end
+    end 
   end
 
-  #can find an artist by name
-  def self.find_by_name(name)
-    #@@artist.detect{|a| a.name == name}
-    all.detect{|a| a.name == name}
-  end
+  module InstanceMethod
+    #converts its name to a url friendly parameter
+    def to_param
+      name.downcase.gsub(' ', '-')
+    end
 
-end
+  end 
+
 
 end
