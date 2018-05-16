efe442ccc8ac4bc9cdd6020592f4135c238e2727
Daksha Patel <dakshapatel100@gmail.com>
2018-05-16 16:40:23 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -4,25 +4,20 @@ module Findable
   module ClassMethods
     def find_by_name
       #can find a song by name
-      def self.find_by_name(name)
+      def find_by_name(name)
         #@@songs.detect{|a| a.name == name}
         all.detect{|a| a.name == name}
       end
 
       #can find an artist by name
-      def self.find_by_name(name)
+      def find_by_name(name)
         #@@artist.detect{|a| a.name == name}
         all.detect{|a| a.name == name}
       end
     end
   end
 
-  module InstanceMethod
-    #converts its name to a url friendly parameter
-    def to_param
-      name.downcase.gsub(' ', '-')
-    end
-  end
+
 
 
 end
