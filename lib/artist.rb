9d4cd8623a7b1b3066f0f32ff0f4482c2ebd0ede
Daksha Patel <dakshapatel100@gmail.com>
2018-05-16 15:35:55 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,32 +14,32 @@ class Artist
       @songs = []
     end
 
-#detect artist in the artists array where the
-#artist name is the name
+#can find an artist by name
   def self.find_by_name(name)
     @@artists.detect{|a| a.name == name}
   end
 
 
 
-  def self.all
+  def self.all3
     #return artist array
+    #keeps track of the artists that have been created
     @@artists
   end
   #####
   def self.reset_all
-    #clear artists
+    #can reset the artists that have been created
     self.all.clear
   end
   #####
   def self.count
-    #count the artists in the artist array
+    #can count how many artists have been created
     #@@artists.count
     self.all.count
   end
 
   def add_song(song)
-    #add song to the songs array
+    #can have a song added
     @songs << song
     #set the artist to the song
     song.artist = self
@@ -47,11 +47,11 @@ class Artist
 
   def add_songs(songs)
     #iterate through each song and add the song
-    songs.each { |song| add_song(song) }
+    ongs.each { |song| add_song(song) }
   end
 
   def to_param
-
+    #converts its name to a url friendly parameter
     name.downcase.gsub(' ', '-')
   end
 
