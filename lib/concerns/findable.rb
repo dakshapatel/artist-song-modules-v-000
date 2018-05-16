d781d860a5bc09c5fc3fcef0d92b849f77a540ad
Daksha Patel <dakshapatel100@gmail.com>
2018-05-16 16:27:30 +0000

    automatically backed up by learn

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,20 @@
+module Findable
+
+
+
+def find_by_name
+  #can find a song by name
+  def self.find_by_name(name)
+    #@@songs.detect{|a| a.name == name}
+    all.detect{|a| a.name == name}
+  end
+
+  #can find an artist by name
+  def self.find_by_name(name)
+    #@@artist.detect{|a| a.name == name}
+    all.detect{|a| a.name == name}
+  end
+
+end
+
+end
